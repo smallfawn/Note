fad8971075a13c2a6e6dd55406ae0ddc536bcf95
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-07-16 11:17:52 +0800

    Update airscript.js

--- JavaScript/airscript.js
+++ JavaScript/airscript.js
@@ -1,23 +1,37 @@
-let resp = HTTP.fetch("https://fastly.jsdelivr.net/gh/smallfawn/Note@main/Notice.json", {
-  method: "GET",
-})
+main()
+function main() {
+    sendNotify()
+}
+function Notice() {
+    let url = "https://fastly.jsdelivr.net/gh/smallfawn/Note@main/Notice.json"
+    let options = {
+        method: "GET",
+        headers: {},
+    }
+    let result = httpRequest(url, options)
+    console.log(result.notice);
+    return result.notice
+}
+function sendNotify() {
+    let content = Notice()
+    let body = {
+        token: "",
+        title: "来自airScript的消息通知",
+        content: content,
+        topic: "",
+    };
+    let url = "https://www.pushplus.plus/send"
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body),
+    }
+    let result = httpRequest(url, options)
+    console.log(result);
+}
 
-//console.log(resp.json())
-let q = resp.json()
-console.log(q)
-let w = JSON.stringify(q)
-console.log(w)
-let body = {
-  token: "",
-  title: "来自airScript的消息通知",
-  content: w,
-  topic: "",
-};
-console.log(body)
-const r = HTTP.fetch("https://www.pushplus.plus/send", {
-  method: 'POST',
-  body: JSON.stringify(body),
-  headers: {
-    'Content-Type': ' application/json',
-  },
-})
+function httpRequest(url, options) {
+    return HTTP.fetch(url, options).json()
+}
