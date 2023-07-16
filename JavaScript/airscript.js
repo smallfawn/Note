c569486effb09310c65838ecb1b0d23284f3149e
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-07-16 12:03:23 +0800

    Update airscript.js

--- JavaScript/airscript.js
+++ JavaScript/airscript.js
@@ -1,10 +1,8 @@
 const $ = Env("")
-let msg = ""
-
 main()
 function main() {
     Notice()
-    sendNotify()
+    $.sendNotify()
 }
 function Notice() {
     let url = "https://fastly.jsdelivr.net/gh/smallfawn/Note@main/Notice.json"
@@ -16,37 +14,36 @@ function Notice() {
     $.DoubleLog(result.notice)
 }
 
-
-function sendNotify() {
-    let body = {
-        token: "",
-        title: "来自airScript的消息通知",
-        content: msg,
-        topic: "",
-    };
-    let url = "https://www.pushplus.plus/send"
-    let options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body),
-    }
-    let result = $.httpRequest(url, options)
-    console.log(result);
-}
 function Env() {
     const env = {};
     // 定义属性
     env.property = "value";
     // 定义方法
+    env.msg = ""
     env.DoubleLog = function (message) {
         console.log(message);
-        msg += `\n ${message}`
+        this.msg += `\n ${message}`
     }
     env.httpRequest = function (url, options) {
         return HTTP.fetch(url, options).json();
     }
-    
+    env.sendNotify = function () {
+        let body = {
+            token: "",
+            title: "来自airScript的消息通知",
+            content: this.msg,
+            topic: "",
+        };
+        let url = "https://www.pushplus.plus/send"
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body),
+        }
+        let result = this.httpRequest(url, options)
+        console.log(result);
+    }
     return env;
 }
