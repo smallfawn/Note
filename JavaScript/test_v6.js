8625cbf6496c5b473d60037a17a1679fc74ff4dd
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-06-09 09:30:46 +0800

    Update test_v6.js

--- JavaScript/test_v6.js
+++ JavaScript/test_v6.js
@@ -25,68 +25,68 @@ hostname =
 const $ = new Env("测试");
 let ckName = `test`;
 let userCookie = checkEnv(
-  ($.isNode() ? process.env[ckName] : $.getdata(ckName)) || ""
+    ($.isNode() ? process.env[ckName] : $.getdata(ckName)) || ""
 );
 const notify = $.isNode() ? require("./sendNotify") : "";
 
 !(async () => {
-  console.log(
-    `==================================================\n 脚本执行 - 北京时间(UTC+8): ${new Date(
-      new Date().getTime() +
-        new Date().getTimezoneOffset() * 60 * 1000 +
-        8 * 60 * 60 * 1000
-    ).toLocaleString()} \n==================================================`
-  );
-  //console.log(userCookie)
-  if (!userCookie?.length) return console.log(`没有找到CK哦`);
-  let index = 1;
-  let strSplitor = "#";
+    console.log(
+        `==================================================\n 脚本执行 - 北京时间(UTC+8): ${new Date(
+            new Date().getTime() +
+            new Date().getTimezoneOffset() * 60 * 1000 +
+            8 * 60 * 60 * 1000
+        ).toLocaleString()} \n==================================================`
+    );
+    //console.log(userCookie)
+    if (!userCookie?.length) return console.log(`没有找到CK哦`);
+    let index = 1;
+    let strSplitor = "#";
 
-  for (let user of userCookie) {
-    $.log(`\n🚀 user:【${index }】 start work\n`);
-    index++
-    $.id = user.split(strSplitor)[0];
-    $.ckStatus = true;
-  }
+    for (let user of userCookie) {
+        $.log(`\n🚀 user:【${index}】 start work\n`);
+        index++
+        $.id = user.split(strSplitor)[0];
+        $.ckStatus = true;
+    }
 
-  await $.sendMsg($.logs.join("\n"));
+    await $.sendMsg($.logs.join("\n"));
 })()
-  .catch((e) => console.log(e))
-  .finally(() => $.done());
+    .catch((e) => console.log(e))
+    .finally(() => $.done());
 
 
 
 function checkEnv(userCookie) {
-  const envSplitor = ["&", "\n"];
-  console.log(userCookie);
-  let userList = userCookie
-    .split(envSplitor.find((o) => userCookie.includes(o)) || "&")
-    .filter((n) => n);
-  console.log(`共找到${userList.length}个账号`);
-  return userList;
+    const envSplitor = ["&", "\n"];
+    console.log(userCookie);
+    let userList = userCookie
+        .split(envSplitor.find((o) => userCookie.includes(o)) || "&")
+        .filter((n) => n);
+    console.log(`共找到${userList.length}个账号`);
+    return userList;
 }
 // prettier-ignore
 function Env(t, s) { return new (class { constructor(t, s) { this.name = t; this.logs = []; this.logSeparator = "\n"; this.startTime = new Date().getTime(); Object.assign(this, s); this.log("", `\ud83d\udd14${this.name},\u5f00\u59cb!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } queryStr(options) { return Object.entries(options).map(([key, value]) => `${key}=${typeof value === "object" ? JSON.stringify(value) : value}`).join("&") } getURLParams(url) { const params = {}; const queryString = url.split("?")[1]; if (queryString) { const paramPairs = queryString.split("&"); paramPairs.forEach((pair) => { const [key, value] = pair.split("="); params[key] = value }) } return params } isJSONString(str) { try { return JSON.parse(str) && typeof JSON.parse(str) === "object" } catch (e) { return false } } isJson(obj) { var isjson = typeof obj == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length; return isjson } async sendMsg(message) { if (!message) return; if (this.isNode()) { await notify.sendNotify(this.name, message) } else { this.msg(this.name, "", message) } } randomNumber(length) { const characters = "0123456789"; return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join("") } randomString(length) { const characters = "abcdefghijklmnopqrstuvwxyz0123456789"; return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join("") } uuid() { return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) { var r = (Math.random() * 16) | 0, v = c == "x" ? r : (r & 0x3) | 0x8; return v.toString(16) }) } time(t) { let s = { "M+": new Date().getMonth() + 1, "d+": new Date().getDate(), "H+": new Date().getHours(), "m+": new Date().getMinutes(), "s+": new Date().getSeconds(), "q+": Math.floor((new Date().getMonth() + 3) / 3), S: new Date().getMilliseconds(), }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (new Date().getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in s) { new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? s[e] : ("00" + s[e]).substr(("" + s[e]).length))) } return t } msg(title = t, subtitle = "", body = "", options) { const formatOptions = (options) => { if (!options) { return options } else if (typeof options === "string") { if (this.isQuanX()) { return { "open-url": options } } else { return undefined } } else if (typeof options === "object" && (options["open-url"] || options["media-url"])) { if (this.isQuanX()) { return options } else { return undefined } } else { return undefined } }; if (!this.isMute) { if (this.isQuanX()) { $notify(title, subtitle, body, formatOptions(options)) } } let logs = ["", "==============📣系统通知📣=============="]; logs.push(title); subtitle ? logs.push(subtitle) : ""; body ? logs.push(body) : ""; console.log(logs.join("\n")); this.logs = this.logs.concat(logs) } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, s) { const e = !this.isQuanX(); e ? this.log("", `\u2757\ufe0f${this.name},\u9519\u8bef!`, t.stack) : this.log("", `\u2757\ufe0f${this.name},\u9519\u8bef!`, t) } wait(t) { return new Promise((s) => setTimeout(s, t)) } done(t = {}) { const s = new Date().getTime(), e = (s - this.startTime) / 1e3; this.log("", `\ud83d\udd14${this.name},\u7ed3\u675f!\ud83d\udd5b ${e}\u79d2`); this.log(); if (this.isNode()) { process.exit(1) } if (this.isQuanX()) { $done(t) } } })(t, s) }
 
 async function Request(options) {
-  if ($.isNode()) {
-    const axios = require("axios");
-    Request = async (options) => {
-      try {
-        return await axios.request(options);
-      } catch (error) {
-        return error && error.error ? error.error : "请求失败";
-      }
-    };
-  }
-  if ($.isQuanX()) {
-    Request = async (options) => {
-      try {
-        return await $task.fetch(options);
-      } catch (error) {
-        return error && error.error ? error.error : "请求失败";
-      }
-    };
-  }
-  return await Request(options);
+    if ($.isNode()) {
+        const axios = require("axios");
+        Request = async (options) => {
+            try {
+                return await axios.request(options);
+            } catch (error) {
+                return error && error.error ? error.error : "请求失败";
+            }
+        };
+    }
+    if ($.isQuanX()) {
+        Request = async (options) => {
+            try {
+                return await $task.fetch(options);
+            } catch (error) {
+                return error && error.error ? error.error : "请求失败";
+            }
+        };
+    }
+    return await Request(options);
 }
