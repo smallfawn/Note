f01291fc685a08c1fd0d426b6a3101c489b01525
smallfawn <860562056@qq.com>
2025-03-08 09:25:25 +0800

    Update updateInvite.js

--- action/updateInvite.js
+++ action/updateInvite.js
@@ -37,10 +37,10 @@ async function freeLotteryInfo() {
         console.log(`上上签抽奖列表查询失败[${res.msg}]`);
     }
 }
-async function fetchInviteData() {
+async function fetchInviteData(id) {
 
     let data = JSON.stringify({
-        "id": 32000,
+        "id": id,
         "type": 2
     });
 
@@ -50,7 +50,7 @@ async function fetchInviteData() {
         headers: {
             'Content-Type': 'application/json',
 
-            'x-auth-token': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3NDEzOTQ3MjMsImV4cCI6MTc3MjkzMDcyMywiaXNzIjoiODVjOGRlZmUwMTdlYjJlZiIsInN1YiI6Ijg1YzhkZWZlMDE3ZWIyZWYiLCJ1dWlkIjoiODVjOGRlZmUwMTdlYjJlZiIsInVzZXJJZCI6MTYzMDM2Mjk1OCwidXNlck5hbWUiOiLosYHovr7pu5HpuL3lrZBiNUoiLCJpc0d1ZXN0IjpmYWxzZX0.PyZMqsv_tT9z6cJw595cajpSc5t4_7OqNBpnM_tf_yTo_B4F968Eo68gtWriO57X8L9AQ6Ew-72hiLdVqSLvDAX8Fx8Kc9oPm6eIPVa0yW-OxxoSvjxd76death9rwk8n6tNLkXmZfh-KNK8iLFDS5-St-zpF8TORcvJBra5y2DFjEHwDV2jC82Hick89jHjScUhqY-4z3L2-Xqp9sUBRYN11A6cozkvOvpxmromGW5FPIsyEYK7WnD-v6D_U4UntEd9zMer4j2aG8m4T9cXSZi7D7lfYNQW7eaaZNXLrQXA6sK2LfPxazLMBY4IYbOm_gKYQg_5ueMkPauKFIn7mQ',
+            'x-auth-token': 'Bearer ' + authToken,
 
 
         },
@@ -69,7 +69,10 @@ async function fetchInviteData() {
 
 !(async () => {
     await freeLotteryInfo()
-    await fetchInviteData()
+    for (let id of freeLotteryIds) {
+        await fetchInviteData(id)
+
+    }
     console.log(invite);
 
     fs.writeFileSync('./dwinvite.json', JSON.stringify(invite, null, 2))
