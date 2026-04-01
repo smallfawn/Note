a67bbdf32b0c1b97795c27715ad152dc8883ec9a
smallfawn <101914820+smallfawn@users.noreply.github.com>
2026-04-01 14:27:01 +0800

    Update test_v7.js

--- JavaScript/test_v7.js
+++ JavaScript/test_v7.js
@@ -2,10 +2,16 @@
 ------------------------------------------
 @Author: sm
 @Date: 2024.06.07 19:15
-@Description: 测试
+@Description: 海底捞
 cron: 12 12 * * *
 ------------------------------------------
-#Notice:
+#Notice:   
+如果微信小程序抓包的写变量名hdlwx:
+https://superapp-public.kiwa-tech.com/api/gateway/login/center/login/wechatLogin 接口的请求参数openId#uid
+如果是APP抓包的写变量名hdlapp:
+自助获取token接口：
+
+
 ⚠️【免责声明】
 ------------------------------------------
 1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
@@ -17,52 +23,158 @@ cron: 12 12 * * *
 7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 */
 
-const $ = new Env("北京现代");
-let ckName = `testA`;
+const { Env } = require("../tools/env")
+const $ = new Env("硬声");
+let ckName = `yingsheng`;
 const strSplitor = "#";
-const envSplitor = ["&", "\n"];
-process.env[ckName] = "testA#testB&testC#testD"
-const notify = $.isNode() ? require("./sendNotify") : "";
+process.env[ckName] = "15666655552#sndjdj"
 const axios = require("axios");
 const defaultUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.31(0x18001e31) NetType/WIFI Language/zh_CN miniProgram"
 
 
-class Task  {
-    constructor(env) {
 
+
+class Task {
+    constructor(env) {
         this.index = $.userIdx++
         let user = env.split(strSplitor);
         this.name = user[0];
         this.passwd = user[1];
-    }
-    async request(options) {
-        return await axios.request(options);
+        this.auth = '';
+        this.device_id = '07cdc486c91ca0457e4263cfa9667aa7od'
+        this.valid = false;
 
     }
 
 
-    async run() {
-
-
-
-        console.log(this.index);
-
+    async login() {
+        try {
+            let pwd = this.EncryptCrypto(this.passwd)
+            console.log(pwd)
+            let param = { 'account': this.name, 'password': pwd, 'device_id': this.device_id }
+            let url = `https://ysapi.elecfans.com/api/sso/accountLogin`
+            let body = $.jsonToStr(param, '&', true)
+            let headersParams = this.calculateSign(param, 'ysapi')
+            let options = {
+                method: 'post',
+                url,
+                headers: {
+                    ...headersParams,
+                    "User-Agent": defaultUserAgent,
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                data: body
+            }
+            let { data: result } = await axios.request(options)
+            if (result.code == 0) {
+                this.valid = true
+                this.name = result.data.mobile
+                this.coins = result.data.coins
+                this.auth = result.data.Authorization
+                this.user_id = result.data.user_id
+                console.log(`登录成功`)
+                console.log(`手机：${this.name}`)
+                console.log(`硬币：${this.coins}`)
+            } else {
+                console.log(`登录失败: ${JSON.stringify(result)}`)
+            }
+        } catch (e) {
+            console.log(e)
+        } finally { }
     }
+
+    async getInfo() {
+        try {
+            let param = { 'user_id': this.user_id }
+            let url = `https://ysapi.elecfans.com/api/member/getInfo?${$.jsonToStr(param, '&')}`
+            let headersParams = this.calculateSign(param, 'ysapi')
+            let options = {
+                method: 'get',
+                url,
+                headers: {
+                    ...headersParams,
+                    "User-Agent": defaultUserAgent,
+                },
+            }
+            let { data: result } = await axios.request(options)
+            if (result.code == 0) {
+                this.coins = result.data.coins
+                console.log(`硬币：${this.coins}`)
+            } else {
+                console.log(`查询账户失败: ${result.message}`)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async getSignStatus() {
+        try {
+            let param = { 'date': '' }
+            let url = `https://yingsheng.elecfans.com/ysapi/wapi/activity/signin/data?${$.jsonToStr(param, '&')}`
+            let headersParams = this.calculateSign(param, 'yingsheng')
+            let options = {
+                method: 'get',
+                url,
+                headers: {
+                    ...headersParams,
+                    "User-Agent": defaultUserAgent,
+                },
+            }
+            let { data: result } = await axios.request(options)
+            if (result.code == 0) {
+                if (result.data.data.today_is_sign == 1) {
+                    console.log(`今日已签到`)
+                } else {
+                    console.log(`今日未签到`)
+                    await $.wait(this.TASK_WAIT_TIME);
+                    await this.signin();
+                }
+            } else {
+                console.log(`查询签到状态失败: ${result.message}`)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async signin() {
+        try {
+            let param = { 'date': '' }
+            let url = `https://yingsheng.elecfans.com/ysapi/wapi/activity/signin/signin`
+            let body = JSON.stringify(param)
+            let headersParams = this.calculateSign(param, 'yingsheng')
+            let options = {
+                method: 'post',
+                url,
+                headers: {
+                    ...headersParams,
+                    "User-Agent": defaultUserAgent,
+                    "Content-Type": "application/json"
+                },
+                data: body
+            }
+            let { data: result } = await axios.request(options)
+            if (result.code == 0) {
+                console.log(`签到成功，获得${result.data.coins}硬币`)
+            } else {
+                console.log(`签到失败: ${result.message}`)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
 }
-
 
 !(async () => {
     await getNotice()
     $.checkEnv(ckName);
 
     for (let user of $.userList) {
-        //
-
         await new Task(user).run();
-
     }
-
-
 })()
     .catch((e) => console.log(e))
     .finally(() => $.done());
@@ -77,147 +189,4 @@ async function getNotice() {
     let { data: res } = await axios.request(options);
     $.log(res)
     return res
-}
-
-
-// prettier-ignore
-function Env(t, s) {
-    return new (class {
-        constructor(t, s) {
-            this.userIdx = 1;
-            this.userList = [];
-            this.userCount = 0;
-            this.name = t;
-            this.notifyStr = [];
-            this.logSeparator = "\n";
-            this.startTime = new Date().getTime();
-            Object.assign(this, s);
-            this.log(`\ud83d\udd14${this.name},\u5f00\u59cb!`);
-        }
-        checkEnv(ckName) {
-            let userCookie = (this.isNode() ? process.env[ckName] : "") || "";
-            this.userList = userCookie.split(envSplitor.find((o) => userCookie.includes(o)) || "&").filter((n) => n);
-            this.userCount = this.userList.length;
-            this.log(`共找到${this.userCount}个账号`);
-        }
-        async sendMsg() {
-            this.log("==============📣Center 通知📣==============")
-            for (let i = 0; i < this.notifyStr.length; i++) {
-                if (Object.prototype.toString.call(this.notifyStr[i]) === '[object Object]' ||
-                    Object.prototype.toString.call(this.notifyStr[i]) === '[object Array]') {
-                    this.notifyStr[i] = JSON.stringify(this.notifyStr[i]);
-                }
-            }
-            let message = this.notifyStr.join(this.logSeparator);
-            if (this.isNode()) {
-                await notify.sendNotify(this.name, message);
-            } else {
-            }
-        }
-        isNode() {
-            return "undefined" != typeof module && !!module.exports;
-        }
-
-        queryStr(options) {
-            const queryString = require("querystring");
-            return queryString.stringify(options);
-        }
-        getURLParams(url) {
-            const params = {};
-            const queryString = url.split("?")[1];
-            if (queryString) {
-                const paramPairs = queryString.split("&");
-                paramPairs.forEach((pair) => {
-                    const [key, value] = pair.split("=");
-                    params[key] = value;
-                });
-            }
-            return params;
-        }
-        isJSONString(str) {
-            try {
-                return JSON.parse(str) && typeof JSON.parse(str) === "object";
-            } catch (e) {
-                return false;
-            }
-        }
-        isJson(obj) {
-            var isjson =
-                typeof obj == "object" &&
-                Object.prototype.toString.call(obj).toLowerCase() ==
-                "[object object]" &&
-                !obj.length;
-            return isjson;
-        }
-
-        randomNumber(length) {
-            const characters = "0123456789";
-            return Array.from(
-                { length },
-                () => characters[Math.floor(Math.random() * characters.length)]
-            ).join("");
-        }
-        randomString(length) {
-            const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-            return Array.from(
-                { length },
-                () => characters[Math.floor(Math.random() * characters.length)]
-            ).join("");
-        }
-        uuid() {
-            return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-                /[xy]/g,
-                function (c) {
-                    var r = (Math.random() * 16) | 0,
-                        v = c == "x" ? r : (r & 0x3) | 0x8;
-                    return v.toString(16);
-                }
-            );
-        }
-        time(t) {
-            let s = {
-                "M+": new Date().getMonth() + 1,
-                "d+": new Date().getDate(),
-                "H+": new Date().getHours(),
-                "m+": new Date().getMinutes(),
-                "s+": new Date().getSeconds(),
-                "q+": Math.floor((new Date().getMonth() + 3) / 3),
-                S: new Date().getMilliseconds(),
-            };
-            /(y+)/.test(t) &&
-                (t = t.replace(
-                    RegExp.$1,
-                    (new Date().getFullYear() + "").substr(4 - RegExp.$1.length)
-                ));
-            for (let e in s) {
-                new RegExp("(" + e + ")").test(t) &&
-                    (t = t.replace(
-                        RegExp.$1,
-                        1 == RegExp.$1.length
-                            ? s[e]
-                            : ("00" + s[e]).substr(("" + s[e]).length)
-                    ));
-            }
-            return t;
-        }
-
-        log(content) {
-            this.notifyStr.push(content)
-            console.log(content)
-        }
-        wait(t) {
-            return new Promise((s) => setTimeout(s, t));
-        }
-        async done(t = {}) {
-            await this.sendMsg();
-            const s = new Date().getTime(),
-                e = (s - this.startTime) / 1e3;
-            this.log(
-                `\ud83d\udd14${this.name},\u7ed3\u675f!\ud83d\udd5b ${e}\u79d2`
-            );
-            if (this.isNode()) {
-                process.exit(1);
-            }
-        }
-    })(t, s);
 }
