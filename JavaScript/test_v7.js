f3d5300adc6a0858149c969dd02b8f482eeb9430
smallfawn <101914820+smallfawn@users.noreply.github.com>
2026-04-08 13:58:53 +0800

    Update test_v7.js

--- JavaScript/test_v7.js
+++ JavaScript/test_v7.js
@@ -3,23 +3,15 @@
 @Author: sm
 @Date: 2024.06.07 19:15
 @Description:  
-cron: 30 8 * * *
+@cron: 30 8 * * *
 ------------------------------------------
-#Notice:   
 
-⚠️【免责声明】
 ------------------------------------------
-1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
-2、由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
-3、请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
-4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
-5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
-6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
-7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
+
 */
 
 const {
-	Env
+    Env
 } = require("../tools/env")
 const $ = new Env("babycare");
 let ckName = `babycare`;
@@ -30,45 +22,45 @@ const defaultUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X)
 
 
 class Task {
-	constructor(env) {
-		this.index = $.userIdx++
-		this.user = env.split(strSplitor);
-		this.token = this.user[0];
+    constructor(env) {
+        this.index = $.userIdx++
+        this.user = env.split(strSplitor);
+        this.token = this.user[0];
 
-	}
+    }
 
-	async run() {
+    async run() {
 
-		await this.signIn()
-	}
+        await this.signIn()
+    }
 
-	async signIn() {
-		let options = {
-			method: 'POST',
-			url: `https://api.bckid.com.cn/operation/front/bonus/userSign/v3/sign`,
-			headers: {
-				'Host': 'api.bckid.com.cn',
-				'authorization': this.token,
-				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b11) XWEB/9129',
-			},
-			data: {
+    async signIn() {
+        let options = {
+            method: 'POST',
+            url: `https://api.bckid.com.cn/operation/front/bonus/userSign/v3/sign`,
+            headers: {
+                'Host': 'api.bckid.com.cn',
+                'authorization': this.token,
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b11) XWEB/9129',
+            },
+            data: {
 
-			}
-		};
-		let {
-			data: result
-		} = await axios.request(options);
-		if (result?.code == '200') {
-			//打印签到结果
-			$.log(`🌸账号[${this.index}]` + `🕊当前已签到${result.body.signDaysCountMod}天🎉`);
-		} else {
-			$.log(`🌸账号[${this.index}] 签到-失败:${result.message || result.msg }❌`)
-		}
-
-
+            }
+        };
+        let {
+            data: result
+        } = await axios.request(options);
+        if (result?.code == '200') {
+            //打印签到结果
+            $.log(`🌸账号[${this.index}]` + `🕊当前已签到${result.body.signDaysCountMod}天🎉`);
+        } else {
+            $.log(`🌸账号[${this.index}] 签到-失败:${result.message || result.msg}❌`)
+        }
 
 
-	}
+
+
+    }
 
 
 
@@ -80,30 +72,37 @@ class Task {
 }
 
 !(async () => {
-	await getNotice()
-	$.checkEnv(ckName);
+    await getNotice()
+    $.checkEnv(ckName);
 
-	for (let user of $.userList) {
-		await new Task(user).run();
-	}
+    for (let user of $.userList) {
+        await new Task(user).run();
+    }
 })()
-.catch((e) => console.log(e))
-	.finally(() => $.done());
+    .catch((e) => console.log(e))
+    .finally(() => $.done());
 
 async function getNotice() {
-	try {
-		let options = {
-			url: `https://ghproxy.net/https://raw.githubusercontent.com/smallfawn/Note/refs/heads/main/Notice.json`,
-			headers: {
-				"User-Agent": defaultUserAgent,
-			},
-            timeout:3000
-		}
-		let {
-			data: res
-		} = await axios.request(options);
-		$.log(res)
-		return res
-	} catch (e) {}
+    console.log(`1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
+2、由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
+3、请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
+4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
+5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
+6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
+7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。`)
+    try {
+        let options = {
+            url: `https://ghproxy.net/https://raw.githubusercontent.com/smallfawn/Note/refs/heads/main/Notice.json`,
+            headers: {
+                "User-Agent": defaultUserAgent,
+            },
+            timeout: 3000
+        }
+        let {
+            data: res
+        } = await axios.request(options);
+        $.log(res)
+        return res
+    } catch (e) { }
 
 }
